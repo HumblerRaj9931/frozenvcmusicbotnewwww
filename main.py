29b14aa2ecb4d61bd4a57289530daf9bee84f076
HumblerRaj9931 <vinitnishad197@gmail.com>
2025-08-18 02:35:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -398,7 +398,7 @@ async def start_handler(_, message):
     reply_markup = InlineKeyboardMarkup(buttons)
 
     await message.reply_animation(
-        animation="https://envs.sh/u3o.jpg/IMG20250817833.jpg",
+        animation="https://www.kapwing.com/videos/689eb8aaa886709519016b08",
         caption=caption,
         parse_mode=ParseMode.MARKDOWN,
         reply_markup=reply_markup
